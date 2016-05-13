d468877dfab763db25ea3e96c1ee109026093f49
xieminqiang <13430969936@163.com>
2016-05-13 14:02:50 +0800

    新特性的bug

--- weibo/Classes/Newfeature(新特性)/Controller/MQNewfeatureViewController.m
+++ weibo/Classes/Newfeature(新特性)/Controller/MQNewfeatureViewController.m
@@ -1,201 +1,128 @@
 //
-//高仿微博
-//作者：强子哥哥
-//https://github.com/xieminqiang/weibo
+//  MQNewfeatureViewController.m
+//  weibo
 //
-
-#define HMNewfeatureImageCount 4
+//  Created by 谢明强 on 15/10/31.
+//  Copyright © 2015年 MQ. All rights reserved.
+//
 
 #import "MQNewfeatureViewController.h"
 #import "MQTabBarViewController.h"
-
-@interface MQNewfeatureViewController () <UIScrollViewDelegate>
-@property (nonatomic, weak) UIPageControl *pageControl;
+#define MQImagecount 4
+@interface MQNewfeatureViewController ()<UIScrollViewDelegate>
+@property (nonatomic , weak) UIPageControl *pagecontrol;
 @end
 
 @implementation MQNewfeatureViewController
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
-    
-    // 1.添加UISrollView
-    [self setupScrollView];
-    
-    // 2.添加pageControl
-    [self setupPageControl];
-}
 
-/**
- *  添加UISrollView
- */
-- (void)setupScrollView
-{
-    // 1.添加UISrollView
-    UIScrollView *scrollView = [[UIScrollView alloc] init];
-    scrollView.frame = self.view.bounds;
+    [self addScrollview];
+    [self addPagecontrol];
+}
+- (void)addScrollview {
+    UIScrollView *scrollView = [[UIScrollView alloc]init];
     scrollView.delegate = self;
+    scrollView.backgroundColor = [UIColor whiteColor];
+    scrollView.frame = self.view.bounds;
     [self.view addSubview:scrollView];
-    
-    // 2.添加图片
     CGFloat imageW = scrollView.width;
     CGFloat imageH = scrollView.height;
-    for (int i = 0; i<HMNewfeatureImageCount; i++) {
-        // 创建UIImageView
-        UIImageView *imageView = [[UIImageView alloc] init];
-        NSString *name = [NSString stringWithFormat:@"new_feature_%d", i + 1];
-        if (FourInch) { // 4inch  需要手动去加载4inch对应的-568h图片
-            name = [name stringByAppendingString:@"-568h"];
+    for (int i =0; i<MQImagecount; i++) {
+        UIImageView *imageview = [[UIImageView alloc] init];
+        imageview.x = imageW*i;
+        imageview.y = 0;
+        imageview.width = imageW;
+        imageview.height = imageH;
+        NSString *name = [NSString stringWithFormat:@"new_feature_%d",i+1];
+        imageview.image = [UIImage imageNamed:name];
+
+        [scrollView addSubview:imageview];
+
+        if (i==MQImagecount-1) {
+            [self setuplastButton:(imageview)];
+
         }
-        imageView.image = [UIImage imageWithName:name];
-        [scrollView addSubview:imageView];
-        
-        // 设置frame
-        imageView.y = 0;
-        imageView.width = imageW;
-        imageView.height = imageH;
-        imageView.x = i * imageW;
-        
-        // 给最后一个imageView添加按钮
-        if (i == HMNewfeatureImageCount - 1) {
-            [self setupLastImageView:imageView];
-        }
+
     }
-    
-    // 3.设置其他属性
-    scrollView.contentSize = CGSizeMake(HMNewfeatureImageCount * imageW, 0);
+
+
+    scrollView.contentSize = CGSizeMake(imageW*MQImagecount, 0);
     scrollView.pagingEnabled = YES;
     scrollView.showsHorizontalScrollIndicator = NO;
-    scrollView.backgroundColor = HMColor(246, 246, 246);
-#warning 别在scrollView.subviews中通过索引来查找对应的子控件
-//    [scrollView.subviews lastObject];
+
 }
 
-/**
- *  添加pageControl
- */
-- (void)setupPageControl
-{
-    // 1.添加
-    UIPageControl *pageControl = [[UIPageControl alloc] init];
-    pageControl.numberOfPages = HMNewfeatureImageCount;
-    pageControl.centerX = self.view.width * 0.5;
-    pageControl.centerY = self.view.height - 30;
-    [self.view addSubview:pageControl];
-
-    // 2.设置圆点的颜色
-    pageControl.currentPageIndicatorTintColor = HMColor(253, 98, 42); // 当前页的小圆点颜色
-    pageControl.pageIndicatorTintColor = HMColor(189, 189, 189); // 非当前页的小圆点颜色
-    self.pageControl = pageControl;
+- (void)setuplastButton:(UIImageView *)imageview{
+    imageview.userInteractionEnabled = YES;
+    [self setupstartButton:imageview];
+    [self setupshareButton:imageview];
 }
 
-/**
- 设置最后一个UIImageView中的内容
- */
-- (void)setupLastImageView:(UIImageView *)imageView
-{
-    imageView.userInteractionEnabled = YES;
-    
-    // 1.添加开始按钮
-    [self setupStartButton:imageView];
-    
-    // 2.添加分享按钮
-    [self setupShareButton:imageView];
-}
+- (void)setupshareButton:(UIImageView *)imageview {
 
-/**
- *  添加分享按钮
- */
-- (void)setupShareButton:(UIImageView *)imageView
-{
-    // 1.添加分享按钮
-    UIButton *shareButton = [[UIButton alloc] init];
-    [imageView addSubview:shareButton];
-    
-    // 2.设置文字和图标
-    [shareButton setTitle:@"分享给大家" forState:UIControlStateNormal];
-    [shareButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-    [shareButton setImage:[UIImage imageWithName:@"new_feature_share_false"] forState:UIControlStateNormal];
-    [shareButton setImage:[UIImage imageWithName:@"new_feature_share_true"] forState:UIControlStateSelected];
-    // 监听点击
-    [shareButton addTarget:self action:@selector(share:) forControlEvents:UIControlEventTouchUpInside];
-    
-    // 3.设置frame
-    shareButton.size = CGSizeMake(150, 35);
-    shareButton.centerX = self.view.width * 0.5;
-    shareButton.centerY = self.view.height * 0.7;
-    
-    // 4.设置间距
-    // top left bottom right
-    // 内边距 == 自切
-    // 被切掉的区域就不能显示内容了
-    // contentEdgeInsets : 切掉按钮内部的内容
-    // imageEdgeInsets : 切掉按钮内部UIImageView的内容
-    // titleEdgeInsets : 切掉按钮内部UILabel的内容
-    shareButton.titleEdgeInsets = UIEdgeInsetsMake(0, 10, 0, 0);
-}
+    UIButton *sharebutton = [[UIButton alloc] init];
 
-/**
- 分享
- */
-- (void)share:(UIButton *)shareButton
-{
-    shareButton.selected = !shareButton.isSelected;
-}
+    [sharebutton setTitleColor:[UIColor blackColor] forState:(UIControlStateNormal)];
+    [sharebutton setTitle:@"给大家分享" forState:(UIControlStateNormal)];
+    [sharebutton setImage:[UIImage imageNamed:@"new_feature_share_false"] forState:(UIControlStateNormal)];
 
-/**
- *  添加开始按钮
- */
-- (void)setupStartButton:(UIImageView *)imageView
-{
-    // 1.添加开始按钮
-    UIButton *startButton = [[UIButton alloc] init];
-    [imageView addSubview:startButton];
-    
-    // 2.设置背景图片
-    [startButton setBackgroundImage:[UIImage imageWithName:@"new_feature_finish_button"] forState:UIControlStateNormal];
-    [startButton setBackgroundImage:[UIImage imageWithName:@"new_feature_finish_button_highlighted"] forState:UIControlStateHighlighted];
-    
-    // 3.设置frame
-    startButton.size = startButton.currentBackgroundImage.size;
-    startButton.centerX = self.view.width * 0.5;
-    startButton.centerY = self.view.height * 0.8;
-    
-    // 4.设置文字
-    [startButton setTitle:@"开始微博" forState:UIControlStateNormal];
-    [startButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
-    [startButton addTarget:self action:@selector(start) forControlEvents:UIControlEventTouchUpInside];
-}
+    [sharebutton setImage:[UIImage imageNamed:@"new_feature_share_true"] forState:(UIControlStateSelected)];
+    sharebutton.size = CGSizeMake(150, 35);
+    sharebutton.centerX = self.view.width * 0.5;
+    sharebutton.centerY = self.view.height * 0.7;
 
-/**
- *  开始微博
- */
-- (void)start
-{
-    [UIApplication sharedApplication].statusBarHidden = NO;
-    
-    // 显示主控制器（HMTabBarController）
-    MQTabBarViewController *vc = [[MQTabBarViewController alloc] init];
-    
-    // 切换控制器
+    [imageview addSubview:sharebutton];
+    [sharebutton addTarget:self action:@selector(share:) forControlEvents:(UIControlEventTouchUpInside)];
+
+}
+- (void)setupstartButton:(UIImageView *)imageview {
+    UIButton *startbutton = [[UIButton alloc] init];
+
+    [startbutton setBackgroundImage:[UIImage imageNamed:@"new_feature_finish_button"] forState:(UIControlStateNormal)];
+    [startbutton setBackgroundImage:[UIImage imageNamed:@"new_feature_finish_button_highlighted"] forState:(UIControlStateHighlighted)];
+    [startbutton setTitle: @"开始微博" forState:(UIControlStateNormal)];
+    [startbutton setTitleColor:[UIColor blackColor] forState:(UIControlStateNormal)];
+    startbutton.size = startbutton.currentBackgroundImage.size;
+    startbutton.centerX = self.view.width * 0.5;
+    startbutton.centerY = self.view.height *0.77;
+    [imageview addSubview:startbutton];
+    [startbutton addTarget:self action:@selector(start) forControlEvents:(UIControlEventTouchUpInside)];
+
+
+}
+- (void)start {
+
+    MQTabBarViewController *nv = [[MQTabBarViewController alloc] init];
     UIWindow *window = [UIApplication sharedApplication].keyWindow;
-    window.rootViewController = vc;
+    window.rootViewController = nv
+    ;
+}
+- (void)share:(UIButton *)sharebutton {
+
+    sharebutton.selected = !sharebutton.selected;
+
+
 }
 
-#pragma mark - UIScrollViewDelegate
-- (void)scrollViewDidScroll:(UIScrollView *)scrollView
-{
-    // 获得页码
-    CGFloat doublePage = scrollView.contentOffset.x / scrollView.width;
-    int intPage = (int)(doublePage + 0.5);
+- ( void)addPagecontrol {
+    UIPageControl *page = [[UIPageControl alloc] init];
+    page.centerX = self.view.width*0.5;
+    page.centerY = self.view.height - 30;
+    page.numberOfPages = MQImagecount;
+    page.pageIndicatorTintColor = [UIColor grayColor];
+    page.currentPageIndicatorTintColor = [UIColor redColor];
+    [self.view addSubview:page];
+    self.pagecontrol = page;
+
+
+}
+-(void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView{
+
+    int doublepage = scrollView.contentOffset.x/scrollView.width;
     
-    // 设置页码
-    self.pageControl.currentPage = intPage;
+    int page = (int)(doublepage +0.5);
+    self.pagecontrol.currentPage = page;
 }
-
-- (void)dealloc
-{
-    HMLog(@"dealloc-------");
-}
-
 @end
